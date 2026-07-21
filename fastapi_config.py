0c200c1eb6382e99154f4150ab618443cae4b230
ONESO-goat <cylean34@gmail.com>
2026-07-21 10:27:01 -0400

    feat: Added cors for fastapi

--- fastapi_config.py
+++ fastapi_config.py
@@ -1,9 +1,16 @@
-# uvicorn main:app --reload
+# uvicorn main:app --reload 8000
 import fastapi
 from sqlmodel import Session, create_engine, SQLModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = fastapi.FastAPI(title="Stora API")
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Replace "*" with your specific https://xxx.lovable.app domain in production
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 DATABASE_URL = "sqlite:///./database.db"
 engine = create_engine(DATABASE_URL, echo=True)
 
